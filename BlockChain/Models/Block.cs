03b7fa6862b3aa6110ed929a773d58715bae52e7
Zakhar <hxgdjahdbdnsjsbdbsnsbs@gmail.com>
2026-05-24 21:55:01 +0300

    Add project files.

---
+++ BlockChain/Models/Block.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain.Models
+{
+    public class Block
+    {
+        // The index of the block in the blockchain
+        public int Index { get; set; }
+
+        // Date and time of the block creation
+        public DateTime Timestamp { get; set; }
+        public string Data { get; set; }
+
+        // Hash of the current block, which is created on base of the data and other properties
+        public string Hash { get; set; }
+        public string PrevHash { get; set; }
+
+        public Block(int index, string data, string prevHash, DateTime timestamp)
+        {
+            Index = index;
+            Timestamp = timestamp;
+            Data = data;
+            PrevHash = prevHash;
+            Hash = "";
+        }
+
+        public Block()
+        {
+
+        }
+    }
+}
